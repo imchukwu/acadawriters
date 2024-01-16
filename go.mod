6cbae9ace1cc05ea0f1832cd68a4af2de0d5a3c3
Michael Chukwu <imchukwu@gmail.com>
2024-01-16 21:10:37 +0100

    update mysql for deployment

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.19 // indirect
+	github.com/xo/dburl v0.20.2
 	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,6 +1,10 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/xo/dburl"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,17 +14,61 @@ var (
 )
 
 func Connect() {
-	// dsn := "root:password@tcp(127.0.0.1:3306)/acadawriters?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := "mysql://root:-dCB1Ad6HdHddCGH3BdEcECgggbDh4gb@roundhouse.proxy.rlwy.net:20981/acadawriters"
+    mysqlURL, ok := os.LookupEnv("MYSQL_URL")
+    if ok == false {
+        fmt.Println("MYSQL_URL not found")
+        return
+    }
 
-	// dsn := "ba15d8eef097fd:bc99a3bc@tcp(us-cdbr-east-06.cleardb.net:3306)/heroku_60b5ede111b594a?charset=utf8mb4&parseTime=True&loc=Local"
-	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
+    u, err := dburl.Parse(mysqlURL + "?charset=utf8&parseTime=True&loc=Local")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    d, err := gorm.Open(mysql.Open(u.DSN), &gorm.Config{})
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    fmt.Println("connected to database successfully")
+
 	db = d
 }
 
 func GetDB() *gorm.DB {
 	return db
 }
+
+
+// package config
+
+// import (
+// 	"os"
+
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/gorm"
+// )
+
+// var (
+// 	db *gorm.DB
+// )
+
+// func Connect() {
+
+// 	dsn := os.Getenv("MYSQL_URL");
+// 	// dsn := "root:password@tcp(127.0.0.1:3306)/acadawriters?charset=utf8mb4&parseTime=True&loc=Local"
+// 	// dsn := "mysql://root:-dCB1Ad6HdHddCGH3BdEcECgggbDh4gb@roundhouse.proxy.rlwy.net:20981/acadawriters"
+
+// 	// dsn := "ba15d8eef097fd:bc99a3bc@tcp(us-cdbr-east-06.cleardb.net:3306)/heroku_60b5ede111b594a?charset=utf8mb4&parseTime=True&loc=Local"
+// 	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	db = d
+// }
+
+// func GetDB() *gorm.DB {
+// 	return db
+// }
